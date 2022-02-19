f8bc21bd336e23355aead71199381e62b1697a5e
gh4ever <leonedoro@naver.com>
2022-02-19 23:54:03 +0200

    challenge

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,42 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import './Dictionary.css'
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState('')
 
-  function handleSubmit(event) {
+  function handleResponse(response) {
+    console.log(response.data)
+  }
+
+  function search(event) {
     event.preventDefault()
-    alert(`Searching for the ${keyword}...`)
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+    console.log(apiUrl)
+    axios.get(apiUrl).then(handleResponse)
   }
 
   function handleKeywordChange(event) {
-    event.preventDefault()
     setKeyword(event.target.value)
   }
 
   return (
     <div className="Dictionary">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Search for a word"
-          className="form-control search-input"
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <small className="hint">i.e. pink, cheshire, cat, with, stripe</small>
+      <section>
+        <form onSubmit={search}>
+          <label>What words do you want to know?</label>
+          <br />
+          <input
+            type="search"
+            placeholder="Search for a word"
+            defaultValue={props.defaultKeyword}
+            autoFocus={true}
+            className="form-control search-input"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <small className="hint">i.e. pink, cheshire, cat, with, stripe</small>
+      </section>
     </div>
   )
 }

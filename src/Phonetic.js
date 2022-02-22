fbedf023598b4e96764251eb0832a892ba3f2c3b
gh4ever <leonedoro@naver.com>
2022-02-22 16:38:46 +0200

    trial phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
 import './Phonetic.css'
 
 export default function Phonetic(props) {
-  if (props.phonetic) {
-    return (
-      <div className="Phonetic">
-        <a
-          href={props.phonetic.audio}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Listen
-        </a>
-        <h2>{props.phonetic.text}</h2>
-      </div>
-    )
-  } else {
-    return null
-  }
+  return (
+    <div className="Phonectics d-flex flex-row">
+      <AudioPlayer
+        src={props.phonetic.audio}
+        autoPlay={false}
+        customVolumeControls={[]}
+        customAdditionalControls={[]}
+        customProgressBarSection={[]}
+        style={{ width: '80px' }}
+        showJumpControls={false}
+        showFilledProgress={false}
+        showDownloadProgress={false}
+        showSkipControls={false}
+        layout="horizontal-reverse"
+      />
+      <span className="text"> {props.phonetic.text}</span>
+    </div>
+  )
 }

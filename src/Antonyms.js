18fbe956098decd3df2348941aab65d9831a00f6
gh4ever <leonedoro@naver.com>
2022-02-22 22:47:59 +0200

    antonym / synonym js

---
+++ src/Antonyms.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Antonyms.css'
+
+export default function Antonyms(props) {
+    if (props.antonyms) {
+        return (
+            <div className="Antonyms">
+                <strong>Antonyms:</strong>
+
+                <ul>
+                    {props.antonyms.map(function (antonym, index) {
+                        return <li key={index}>{antonym}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    } else {
+        return null
+    }
+}
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,7 @@ export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <div className="Synonyms">
-        <strong>Similar:</strong>
+        <strong>Synonyms:</strong>
 
         <ul>
           {props.synonyms.map(function (synonym, index) {
